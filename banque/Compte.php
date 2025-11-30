4ea38209e3fae835e30d446febe7d60d29e521f2
ANASS AZYZ <anass.azyz.06@gmail.com>
2025-11-30 08:16:24 +0100

    confirme code

--- banque/Compte.php
+++ banque/Compte.php
@@ -1,25 +1,23 @@
 <?php
-    class Compte{
-        private $solde;
-        private $code;
-        private static $dernierCode=0;
 
+class Compte{
+    private $solde;
+    private $code;
+    private static $dernierCode = 0;
 
-        public function __construct($solde=null){
-            $this->code=static::$dernierCode++;
-            $this->solde=$solde;
-        }
-        public function getSolde(){
-            return $this->solde;
-        }
-        public function getCode(){
-            return $this->code;
-        }
-        public function __toString(){
-            $solde=$this->solde ?? 0;
-            return $solde." MAD";
-        }
+    public function __construct($solde=null){
+        $this->code = static::$dernierCode++;
+        $this->solde=$solde;
     }
-
-
-
+    public function getSolde(){
+        return $this->solde;
+    }
+    public function getCode(){
+        return $this->code;
+    }
+    public function __toString(){
+        $solde=$this->solde ?? 0;
+        return $solde." MAD";
+        
+    }
+}
